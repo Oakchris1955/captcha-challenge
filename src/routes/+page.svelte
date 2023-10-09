8d13f0d77382be46775329d222a8592b103e7b15
Oakchris1955 <80592203+Oakchris1955@users.noreply.github.com>
2023-10-09 15:04:37 +0300

    Add settings menu and style buttons
    For now, the only option in the menu is for erasing the guess stats

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -17,6 +17,8 @@
 		}
 	});
 
+	let dialog: HTMLDialogElement;
+
 	let canvas: HTMLCanvasElement;
 	let displayedString = randomString();
 	let inputString: string = '';
@@ -124,6 +126,39 @@
 	});
 </script>
 
+<button id="settingsButton" on:click={() => dialog.showModal()}>
+	<svg xmlns="http://www.w3.org/2000/svg" id="settingsIcon" viewBox="0 0 512 512"
+		><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path
+			d="M495.9 166.6c3.2 8.7 .5 18.4-6.4 24.6l-43.3 39.4c1.1 8.3 1.7 16.8 1.7 25.4s-.6 17.1-1.7 25.4l43.3 39.4c6.9 6.2 9.6 15.9 6.4 24.6c-4.4 11.9-9.7 23.3-15.8 34.3l-4.7 8.1c-6.6 11-14 21.4-22.1 31.2c-5.9 7.2-15.7 9.6-24.5 6.8l-55.7-17.7c-13.4 10.3-28.2 18.9-44 25.4l-12.5 57.1c-2 9.1-9 16.3-18.2 17.8c-13.8 2.3-28 3.5-42.5 3.5s-28.7-1.2-42.5-3.5c-9.2-1.5-16.2-8.7-18.2-17.8l-12.5-57.1c-15.8-6.5-30.6-15.1-44-25.4L83.1 425.9c-8.8 2.8-18.6 .3-24.5-6.8c-8.1-9.8-15.5-20.2-22.1-31.2l-4.7-8.1c-6.1-11-11.4-22.4-15.8-34.3c-3.2-8.7-.5-18.4 6.4-24.6l43.3-39.4C64.6 273.1 64 264.6 64 256s.6-17.1 1.7-25.4L22.4 191.2c-6.9-6.2-9.6-15.9-6.4-24.6c4.4-11.9 9.7-23.3 15.8-34.3l4.7-8.1c6.6-11 14-21.4 22.1-31.2c5.9-7.2 15.7-9.6 24.5-6.8l55.7 17.7c13.4-10.3 28.2-18.9 44-25.4l12.5-57.1c2-9.1 9-16.3 18.2-17.8C227.3 1.2 241.5 0 256 0s28.7 1.2 42.5 3.5c9.2 1.5 16.2 8.7 18.2 17.8l12.5 57.1c15.8 6.5 30.6 15.1 44 25.4l55.7-17.7c8.8-2.8 18.6-.3 24.5 6.8c8.1 9.8 15.5 20.2 22.1 31.2l4.7 8.1c6.1 11 11.4 22.4 15.8 34.3zM256 336a80 80 0 1 0 0-160 80 80 0 1 0 0 160z"
+		/></svg
+	></button
+>
+
+<!-- svelte-ignore a11y-click-events-have-key-events a11y-no-noninteractive-element-interactions -->
+<dialog bind:this={dialog} on:click|self={() => dialog.close()}>
+	<!-- svelte-ignore a11y-no-static-element-interactions -->
+	<div on:click|stopPropagation>
+		<button
+			on:click={() => {
+				if (confirm('Are you sure?')) {
+					window.localStorage.removeItem('guesses');
+					loadGuessStats();
+				}
+
+				dialog.close();
+			}}>Clear guess stats</button
+		>
+
+		<!-- svelte-ignore a11y-autofocus -->
+		<button
+			autofocus
+			on:click={() => {
+				dialog.close();
+			}}>Close</button
+		>
+	</div>
+</dialog>
+
 <!-- Wait for "guesses" object variable to be initialized before displaying this -->
 <div id="counterContainer" class="column">
 	<div>
@@ -191,11 +226,26 @@
 </form>
 
 <style>
-	button:hover {
+	button,
+	input[type='submit'] {
+		background-color: rgb(36, 48, 94);
+		color: rgb(228, 233, 242);
+		border: none;
+		border-radius: 5px;
+
+		margin: 2px;
+		padding: 5px 8px;
+
+		font-size: 0.9rem;
+	}
+
+	button:hover,
+	input[type='submit']:hover {
 		cursor: pointer;
 	}
 
-	button:active {
+	button:active,
+	input[type='submit']:active {
 		cursor: default;
 	}
 
@@ -210,7 +260,7 @@
 	}
 
 	:global(#container),
-	:global(#container) * {
+	:global(#container) *:not(dialog) {
 		display: flex;
 		justify-content: center;
 		align-items: center;
@@ -241,6 +291,43 @@
 		width: 250px;
 
 		background-color: white;
+	}
+
+	dialog {
+		/*background-color: rgb(27, 37, 78);*/
+		background-color: rgb(17, 30, 80);
+
+		border: none;
+		border-radius: 5px;
+		padding: 0;
+	}
+
+	dialog > div {
+		flex-direction: column;
+
+		gap: 3px;
+		padding: 1em;
+	}
+
+	dialog::backdrop {
+		background: rgba(0, 0.3, 0, 0.3);
+		width: 100%;
+	}
+
+	#settingsButton {
+		height: 5%;
+		position: absolute;
+		top: 2.5%;
+		right: 2.5%;
+
+		background-color: transparent;
+		border: 0;
+		padding: 5px;
+	}
+
+	#settingsIcon {
+		height: 100%;
+		width: 100%;
 	}
 
 	#counterContainer {
